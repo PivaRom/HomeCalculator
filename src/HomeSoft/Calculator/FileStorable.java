133018ec1ad898ba80287a2841b75a5c138e3140
Пиванов Роман <pivanov.roman@gmail.com>
2017-02-07 00:10:32 +0200

    Added class StringParser for exclude parseLine

--- src/HomeSoft/Calculator/FileStorable.java
+++ src/HomeSoft/Calculator/FileStorable.java
@@ -4,10 +4,10 @@ package HomeSoft.Calculator;
  * Created by RiP on 04.02.2017.
  */
 public interface FileStorable {
-    public void readFile(String fileName);
-    public void writeFile(String fileName);
-    public boolean existFile(String fileName);
-    public boolean validateFile(String fileName);
-    public <T> T parseLine(String value);
-    public String toFile();
+    void readFile(String fileName);
+    void writeFile(String fileName);
+    boolean existFile(String fileName);
+    boolean validateFile(String fileName);
+    EntityEx parseLine(String value);
+    String toFile();
 }
--- src/HomeSoft/Calculator/StringParser.java
+++
@@ -1,9 +0,0 @@
-package HomeSoft.Calculator;
-
-/**
- * Created by ROMA on 06.02.2017.
- */
-public class StringParser {
-
-
-}
